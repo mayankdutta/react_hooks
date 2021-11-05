8e8c4a49e3831d8b4b7f69ff7269009b11893e94
mayank dutta <smayankdutt@gmail.com>
2021-11-06 02:20:07 +0530

    Login / Logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Login from "./components/Login.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   const [user, setUser] = React.useState("");
-  if (!user) {
-    return <Login setUser={setUser} />;
-  }
-  return <div className="App"> App</div>;
+  if (!user) return <Login setUser={setUser} />;
+
+  React.useEffect(() => {}, [user]);
+  return (
+    <>
+      <Header user={user} setUser={setUser} />
+    </>
+  );
 }
 
 export default App;
