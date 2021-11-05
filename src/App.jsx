70f47ee45f9553ac6d2699e37952b93fb69ac51d
mayankdutta <smayankdutt@gmail.com>
2021-11-06 03:15:53 +0530

    added a controlled component
    responsible for clearing out the input values over time

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import React, { useState, useEffect } from "react";
 import Login from "./components/Login.jsx";
 import Header from "./components/Header.jsx";
+import CreatePost from "./components/CreatePost";
 
 function App() {
   const [user, setUser] = React.useState("");
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    document.title = user === "" ? "Please Login" : `${user}'s Feed`;
+  }, [user]);
+
   if (!user) return <Login setUser={setUser} />;
 
-  React.useEffect(() => {}, [user]);
   return (
     <>
       <Header user={user} setUser={setUser} />
+      <CreatePost user={user} posts={posts} setPosts={setPosts} />
+      {posts.map((post, index) => (
+        <React.Fragment key={index}>
+          {post.image && (
+            <img
+              style={{ height: 200, width: 200, objectFit: "cover" }}
+              src={URL.createObjectURL(post.image)}
+              alt="Post Cover"
+            />
+          )}
+          <p> {post.content}</p>
+          <div> by {post.user}</div>
+        </React.Fragment>
+      ))}
     </>
   );
 }
--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreatePost() {
-  return <div className="App"> Create post</div>;
+function CreatePost({ user, posts, setPosts }) {
+  const [content, setContent] = useState("");
+  const [image, setImage] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const post = { content, image, user };
+    setPosts([post, ...posts]);
+    setContent("");
+  };
+
+  return (
+    <div>
+      <h1> Create post</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add post content"
+          onChange={(event) => setContent(event.target.value)}
+          value={content}
+        />
+        <input
+          type="file"
+          onChange={(event) => setImage(event.target.files[0])}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreatePost;
